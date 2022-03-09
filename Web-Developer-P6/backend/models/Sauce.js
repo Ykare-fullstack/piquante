f1152817b6a72c9aa86300a40738f2308b054616
Alan Assante <probox.alan@gmail.com>
2022-03-09 14:31:04 +0100

    envoi pour résolution bug erreur 404

---
+++ Web-Developer-P6/backend/models/Sauce.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const sauceSchema = mongoose.Schema ({
+
+    userId: { type: String, required: true },
+    name: { type: String, required: true },
+    manufacturer: { type: String, required: true },
+    description: { type: String, required: true },
+    mainPepper: { type: String, required: true },
+    imageUrl: { type: String },
+    heat: { type: Number },
+    likes: { type: Number },
+    dislikes: { type: Number },
+    usersLiked: [{ type: String }],
+    usersDisliked: [{ type: String }],
+});
+
+module.exports = mongoose.model('Sauce', sauceSchema);
